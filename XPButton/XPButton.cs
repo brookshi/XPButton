270613b164dbe8691706b3fb8c0d10d6df399a9b
brookshi <iwxiaot@gmail.com>
2015-10-17 23:04:31 +0800

    remove shadow

--- XPButton/XPButton.cs
+++ XPButton/XPButton.cs
@@ -14,8 +14,6 @@
 //   limitations under the License. 
 #endregion
 
-using Microsoft.Graphics.Canvas.Effects;
-using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
 using System.Collections.Generic;
 using System.Numerics;
@@ -24,8 +22,6 @@ using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
-using Windows.Graphics.Effects;
-using Microsoft.Graphics.Canvas;
 using Windows.Foundation;
 using System.Diagnostics;
 
@@ -35,13 +31,6 @@ namespace XP
     {
         private Grid _shadowGrid;
         private RelativePanel _contentPresenter;
-        private CanvasControl _canvas;
-        private Border _backgroundBorder;
-        private List<Border> _shadowBorders = new List<Border>();
-        private bool _needPrepareShadow = true;
-        double _shadowWidthRate = 0;
-        double _shadowHeightRate = 0;
-
 
         public IconElement Icon
         {
@@ -50,22 +39,6 @@ namespace XP
         }
         public static readonly DependencyProperty IconProperty =
             DependencyProperty.Register("Icon", typeof(IconElement), typeof(XPButton), new PropertyMetadata(null));
-
-        public double ShadowLength
-        {
-            get { return (double)GetValue(ShadowLengthProperty); }
-            set { SetValue(ShadowLengthProperty, value); }
-        }
-        public static readonly DependencyProperty ShadowLengthProperty =
-            DependencyProperty.Register("ShadowLength", typeof(double), typeof(XPButton), new PropertyMetadata(0));
-
-        public Color ShadowColor
-        {
-            get { return (Color)GetValue(ShadowColorProperty); }
-            set { SetValue(ShadowColorProperty, value); }
-        }
-        public static readonly DependencyProperty ShadowColorProperty =
-            DependencyProperty.Register("ShadowColor", typeof(Brush), typeof(XPButton), new PropertyMetadata(Colors.Black));
 
         public double CornerRadius
         {
@@ -80,132 +53,11 @@ namespace XP
         {
             this.DefaultStyleKey = typeof(XPButton);
             Loaded += XPButton_Loaded;
-            SizeChanged += XPButton_SizeChanged;
-        }
-
-        private void _canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
-        {
-            PrepareShadow(sender, args.DrawingSession);
-
-            DrawShadow(sender, args.DrawingSession);
-        }
-
-        void PrepareShadow(CanvasControl canvasCtrl, CanvasDrawingSession drawSession)
-        {
-            if (_needPrepareShadow)
-            {
-                var canvasCommandList = new CanvasCommandList(canvasCtrl);
-                _needPrepareShadow = false;
-                using (var ds = canvasCommandList.CreateDrawingSession())
-                {
-                    ds.FillRoundedRectangle(new Rect(0, 0, ActualWidth, ActualHeight), (float)1, (float)1, Color.FromArgb(76, 0, 0, 0));
-                }
-
-                var topShadowEffect = new Transform2DEffect
-                {
-                    Source = new Transform2DEffect
-                    {
-                        Source = new ShadowEffect
-                        {
-                            BlurAmount = 10,
-                            ShadowColor = Color.FromArgb(180, 0, 0, 0),
-                            Source = canvasCommandList
-                        },
-                    },
-                };
-                var bound = topShadowEffect.GetBounds(drawSession);
-                _shadowWidthRate = (bound.Width - ActualWidth) / 2 / bound.Width;
-                _shadowHeightRate = (bound.Height - ActualHeight) / 2 / bound.Height;
-                canvasCtrl.Invalidate();
-            }
-        }
-
-        int _deep = 1;
-        void DrawShadow(CanvasControl canvasCtrl, CanvasDrawingSession drawSession)
-        {
-            if (!_needPrepareShadow)
-            {
-                var canvasCommandList = new CanvasCommandList(canvasCtrl);
-                using (var ds = canvasCommandList.CreateDrawingSession())
-                {
-                    ds.FillRoundedRectangle(new Rect(_shadowWidthRate * ActualWidth, 0, ActualWidth * (1 - _shadowWidthRate * 2), ActualHeight * ( 1 - _shadowHeightRate)), (float)1, (float)1, Color.FromArgb(76, 0, 0, 0));
-                }
-
-                var shadowEffect = new Transform2DEffect
-                {
-                    Source = new Transform2DEffect
-                    {
-                        Source = new ShadowEffect
-                        {
-                            BlurAmount = 19,
-                            ShadowColor = Color.FromArgb(76, 0, 0, 0),
-                            Source = canvasCommandList
-                        },
-                    },
-                };
-                _contentPresenter.Margin = new Thickness(_shadowWidthRate, 0, _shadowWidthRate, _shadowHeightRate);
-                drawSession.DrawImage(shadowEffect);
-            }
-        }
-
-        private void XPButton_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            //UpdateShadowEffect();
         }
 
         private void XPButton_Loaded(object sender, RoutedEventArgs e)
         {
-            //_contentPresenter.Width = ActualWidth - 2 * ShadowLength / 3;
-            //_contentPresenter.Height = ActualHeight - ShadowLength;
-            _contentPresenter.Margin = new Thickness(ShadowLength / 3, 0, ShadowLength / 3, ShadowLength);
-            _canvas.Draw += _canvas_Draw;
-            //for (int i = 0; i < GetBorderCount(); i++)
-            //{
-            //    var border = new Border() { Background = new SolidColorBrush(Colors.Transparent) };
-            //    _shadowBorders.Add(border);
-            //    _shadowGrid.Children.Add(border);
-            //}
-            // UpdateShadowEffect();
-        }
-
-        int GetBorderCount()
-        {
-            return (int)(GetResolutionScale() * ShadowLength + 0.5);
-        }
-
-        double GetResolutionScale()
-        {
-            return (double)DisplayInformation.GetForCurrentView().ResolutionScale / 100;
-        }
-
-        void UpdateShadowEffect()
-        {
-            var width = Math.Max(0, ActualWidth - ShadowLength * 2);
-            var height = Math.Max(0, ActualHeight - ShadowLength * 2);
-            if (ShadowLength > 0)
-            {
-                var scale = GetResolutionScale();
-                var count = GetBorderCount();
-                var lenGap = ShadowLength / count;
-                var alphaArr = ShadowCalculation.GetShadowValues(count);
-                int index = 0;
-
-                _shadowBorders.ForEach(o =>
-                {
-                    o.Width = ActualWidth - 2 / scale * index;
-                    o.Height = ActualHeight - 2 / scale * index;
-                    o.BorderThickness = new Thickness(1 / scale);
-                    o.CornerRadius = new CornerRadius(index < 3 ? 0 : 2);// (GetCornerRadius(o.Width));
-                    o.BorderBrush = new SolidColorBrush(Color.FromArgb((byte)alphaArr[count - index - 1], ShadowColor.R, ShadowColor.G, ShadowColor.B));
-                    index++;
-                });
-            }
-
-            //_contentPresenter.Width = width;
-            //_contentPresenter.Height = height;
-            _backgroundBorder.Width = width;
-            _backgroundBorder.Height = height;
-            _backgroundBorder.CornerRadius = new CornerRadius(GetCornerRadius(_backgroundBorder.Width));
+            
         }
 
         double GetCornerRadius(double width)
@@ -216,10 +68,7 @@ namespace XP
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            _shadowGrid = (Grid)GetTemplateChild("ShadowGrid");
-            _contentPresenter = (RelativePanel)GetTemplateChild("ContentPanel");
-            _backgroundBorder = (Border)GetTemplateChild("BackgroundBorder");
-            _canvas = (CanvasControl)GetTemplateChild("ShadowCanvas");
+            //_contentPresenter = (RelativePanel)GetTemplateChild("ContentPanel");
         }
     }
 }
